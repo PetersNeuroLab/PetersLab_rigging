ec39499eef0e2f1949d36085460a26c195501ff1
Andy Peters <peters.andrew.j@gmail.com>
2023-03-15 16:09:18 +0000

    Create locations.m

---
+++ +plab/locations.m
@@ -0,0 +1,33 @@
+% Definitions for shared locations across the lab
+
+% TO DO: add method to construct (and search for?) filename
+% - model in AP_cortexlab_filename
+% maybe also something like AP_find_experiments
+
+
+classdef locations
+    properties( Constant = true )
+
+        %% NAS server location
+        server_path = 'Z:\';
+
+        %% Local data paths
+        local_data_path = 'C:\LocalData';
+
+        %% Ports for tcp servers and clients
+        mc_port = 50001
+        timelite_port = 50002
+        mousecam_port = 50003
+
+        %% Github paths
+        github_rigging = 'C:\Github\PetersLab_rigging';
+         
+    end
+
+end
+
+
+
+
+
+
