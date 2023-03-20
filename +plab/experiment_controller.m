f8a8a2839696c97a0d111173026de0c3c52b1ff1
andrada08 <86690938+andrada08@users.noreply.github.com>
2023-03-20 14:08:45 +0000

    Update experiment_controller.m

--- +plab/experiment_controller.m
+++ +plab/experiment_controller.m
@@ -4,19 +4,22 @@ server_timelite = tcpserver("0.0.0.0",plab.locations.timelite_port);
 server_mousecam = tcpserver("0.0.0.0",plab.locations.mousecam_port);
 
 %% send mouse and protocol info
-protocol = 'trial_test';
-name = 'MissMouse0801';
+protocol = 'trial_test.bonsai';
+mouse_name = 'MissMouse0801';
+date_today = string(datetime('today', 'Format', 'yyyy_MM_dd'));
 
 waitfor(server_stim, 'Connected', 1)
-s = struct('mouse', name, 'protocol', protocol);
+s = struct('mouse', mouse_name, 'date', date_today, 'protocol', protocol);
 encode_s = jsonencode(s);
-write(server_stim,encode_s,'string');
+
+writeline(server_stim,encode_s);
 
 %% send start message
 write(server_timelite,  'start', 'string');
 write(server_mousecam,  'start', 'string');
 
 %% send stop message
-write(server_stim,'stop','string');
+writeline(server_stim,'stop');
+
 write(server_timelite, 'stop', 'string');
 write(server_mousecam, 'stop', 'string');
