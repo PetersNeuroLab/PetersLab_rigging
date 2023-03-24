c70ae804ae2a513fdceed2d4a20ac97f7e2f9ab0
andrada08 <86690938+andrada08@users.noreply.github.com>
2023-03-24 14:18:50 +0000

    update name and function for end exp

--- +plab/+exp_control_helpers/read_bonsai_end_exp.m
+++ +plab/+exp_control_helpers/read_bonsai_end_exp.m
@@ -1,13 +1,16 @@
-function readDataFcn(server_stim, ~, server_timelite, server_mousecam, StartButton)
+function read_bonsai_end_exp(server_stim, ~, server_timelite, server_mousecam, StartButton)
     disp('Data received')
     server_stim.UserData = readline(server_stim);
     disp(server_stim.UserData)
+
     if strfind(server_stim.UserData, 'done')
-        % send stop to all servers 
+        % send stop to timelite and mousecam 
         if server_timelite.Connected
             writeline(server_timelite, 'stop');
         end
-%         writeline(server_mousecam, 'stop');
+        if server_mousecam.Connected
+            writeline(server_mousecam, 'stop');
+        end
 
         % change button
         StartButton.Text = 'Start';
