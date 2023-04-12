23a5d763e6dcaf49f0ded1e10876c8c7e1384b5b
andrada08 <86690938+andrada08@users.noreply.github.com>
2023-04-12 18:42:25 +0100

    Update bonsai_server.m

--- +plab/bonsai_server.m
+++ +plab/bonsai_server.m
@@ -125,13 +125,11 @@ function move_data_to_server
     for curr_dir = setdiff({local_data_dirs.name},[".",".."])
     
         curr_dir_local = fullfile(plab.locations.local_data_path,curr_dir);
-        curr_dir_server = fullfile(plab.locations.server_data_path,curr_dir);
     
         fprintf('Copying: %s --> %s \n',curr_dir_local,curr_dir_server)
-    
-        [status,message] = movefile(curr_dir_local,curr_dir_server);
+        [status,message] = movefile(curr_dir_local,plab.locations.server_data_path);
         if ~status
-            warning('Mousecam -- Failed copying to server: %s',message);
+            warning('Bonsai server -- Failed copying to server: %s',message);
         else
             display('Done.')
         end
